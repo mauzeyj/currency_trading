96e148be992c664dff9020d60e07b429b2f47c2a
jason <jason.mauzey@gmail.com>
2020-03-30 06:18:28 -0600

    documented get_data function

--- get_data/get_historical_data.py
+++ get_data/get_historical_data.py
@@ -5,6 +5,14 @@ import pandas as pd
 
 
 def get_data(instrument='EUR_USD', start='2020-03-22', end='2020-03-24', granularity='M1'):
+    """
+    Retrieves data from Oanda, puts candles data into pandas dataframe, sets index to time
+    :param instrument: string: instrument that data is requested for ex
+    :param start: string: year-month-day for the data to start
+    :param end: string: year-month-day for the data to end
+    :param granularity: code for the granularity #todo get a list of these and provide them
+    :return: pandas dataframe
+    """
     config = configparser.ConfigParser()
     config.read('oanda.cfg')
 
